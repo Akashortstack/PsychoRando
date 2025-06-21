e905cddb3360dd6b2ef4ef61375c1e2f38ee80ae
Akashortstack <akashortstack@gmail.com>
2025-06-21 14:57:07 -0700

    watercurse redone, removing faster Milla's Race

--- Scripts/Global/OtherEntities/DisplayVersion.lua
+++ Scripts/Global/OtherEntities/DisplayVersion.lua
@@ -4,7 +4,7 @@ function DisplayVersion(Ob)
     end
 
     Ob.modName = "PsychoRando"
-    Ob.version = "v1.7.1-AP-Branch"
+    Ob.version = "v1.8.0-BETA"
 
     Ob.modNameHandle = nil
     Ob.modVersionHandle = nil
