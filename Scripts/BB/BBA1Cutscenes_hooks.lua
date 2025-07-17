dfe4b8278fc9e85231b4dba695a7983406f6db26
Akashortstack <akashortstack@gmail.com>
2025-07-17 02:58:07 -0700

    Fix Steamertrunk bug in BB, adding RandoSeed.lua to .gitignore

--- Scripts/BB/BBA1Cutscenes_hooks.lua
+++ Scripts/BB/BBA1Cutscenes_hooks.lua
@@ -1,7 +1,9 @@
 function BBA1Cutscenes_hooks(Ob)
     --FULL FUNCTION OVERRIDE
-    --Remove this function completely, no more EmoBaggage cutscene
+    --[[In vanilla, this function is a modified version of the Parent function that will teleport Raz
+    to specific spot after the SteamerTrunk cutscene plays. To avoid this, call the parent function directly and never teleport Raz.
+    ]]
     function Ob:stateCleanupCSEmoBagCollection()
-        return
+		%Ob.Parent.stateCleanupCSEmoBagCollection(self)
     end
 end
